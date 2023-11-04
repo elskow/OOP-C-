479b6fbdc894f850b48209231f2499eff5037274
elskow <elskow@outlook.com>
2023-11-04 17:32:50 +0700

    feat: initiating logic of num to words

--- 6_BilanganTerbilang/Form1.Designer.cs
+++ 6_BilanganTerbilang/Form1.Designer.cs
@@ -28,13 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Font = new System.Drawing.Font("SF UI  Text G 3", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.textBox1.Location = new System.Drawing.Point(36, 108);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(348, 27);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.Font = new System.Drawing.Font("SF UI  Text G 3", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(36, 184);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(572, 31);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("SF UI  Text G 6", 19F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label2.Location = new System.Drawing.Point(30, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(354, 31);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "NumberToWord Converter";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Font = new System.Drawing.Font("SF UI  Text G 3", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(36, 250);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(47, 25);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "ID";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("SF UI  Text G 3", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox2.Location = new System.Drawing.Point(89, 250);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(81, 25);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = "En-Us";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(808, 315);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("SF UI  Text G 3", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "SpelledNumber";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
     }
 }
 
--- 6_BilanganTerbilang/Form1.cs
+++ 6_BilanganTerbilang/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,265 @@ namespace _6_BilanganTerbilang
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private static bool isEnglish = false;
+
+        private static string[] englishMegas = { "", "thousand", "million", "billion", "trillion", "quadrillion", "quintillion", "sextillion", "septillion", "octillion", "nonillion", "decillion", "undecillion", "duodecillion", "tredecillion", "quattuordecillion" };
+        private static string[] englishUnits = { "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+        private static string[] englishTens = { "", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+        private static string[] englishTeens = { "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+
+        private static string[] indonesiaMegas = { "", "ribu", "juta", "milyar", "triliun", "kuadriliun", "kuantiliun", "sekstiliun", "septiliun", "oktiliun", "noniliun", "desiliun", "undesiliun", "duodesiliun", "tredesiliun", "kuatuordesiliun" };
+        private static string[] indonesiaUnits = { "", "satu", "dua", "tiga", "empat", "lima", "enam", "tujuh", "delapan", "sembilan" };
+        private static string[] indonesiaTens = { "", "sepuluh", "dua puluh", "tiga puluh", "empat puluh", "lima puluh", "enam puluh", "tujuh puluh", "delapan puluh", "sembilan puluh" };
+        private static string[] indonesiaTeens = { "sepuluh", "sebelas", "dua belas", "tiga belas", "empat belas", "lima belas", "enam belas", "tujuh belas", "delapan belas", "sembilan belas" };
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                checkBox2.Checked = false;
+                isEnglish = false;
+                textBox1_TextChanged(sender, e);
+            }
+            else
+            {
+                checkBox2.Checked = true;
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+            {
+                checkBox1.Checked = false;
+                isEnglish = true;
+                textBox1_TextChanged(sender, e);
+            }
+            else
+            {
+                checkBox1.Checked = true;
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            checkBox1.Checked = true;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                textBox2.Text = "";
+                return;
+            }
+
+            if (textBox1.Text.Contains(","))
+            {
+                string[] split = textBox1.Text.Split(',');
+
+                if (split.Length != 2 || !long.TryParse(split[0], out long beforeComma) || !long.TryParse(split[1], out long afterComma))
+                {
+                    textBox2.Text = "Input must be an integer";
+                    return;
+                }
+
+                if (isEnglish) textBox2.Text = $"{LongToEnUs(beforeComma)} point {LongToEnUs(afterComma)}";
+                if (!isEnglish) textBox2.Text = $"{LongToID(beforeComma)} koma {LongToID(afterComma)}";
+                return;
+            }
+
+            if (!long.TryParse(textBox1.Text, out long input))
+            {
+                textBox2.Text = "Input must be an integer";
+                return;
+            }
+            if (isEnglish) textBox2.Text = LongToEnUs(input);
+            if (!isEnglish) textBox2.Text = LongToID(input);
+        }
+
+        private static string LongToEnUs(long input)
+        {
+            List<string> words = new List<string>();
+
+            if (input < 0)
+            {
+                words.Add("minus");
+                input *= -1;
+            }
+
+            List<long> triplets = LongToTriplets(input);
+
+            if (triplets.Count == 0)
+            {
+                return "zero";
+            }
+
+            for (int idx = triplets.Count - 1; idx >= 0; idx--)
+            {
+                long triplet = triplets[idx];
+
+                if (triplet == 0)
+                {
+                    continue;
+                }
+
+                long hundreds = triplet / 100 % 10;
+                long tens = triplet / 10 % 10;
+                long units = triplet % 10;
+
+                if (hundreds > 0)
+                {
+                    words.Add(englishUnits[hundreds]);
+                    words.Add("hundred");
+                }
+
+                if (tens == 0 && units == 0)
+                {
+                    goto tripletEnd;
+                }
+
+                switch (tens)
+                {
+                    case 0:
+                        words.Add(englishUnits[units]);
+                        break;
+                    case 1:
+                        words.Add(englishTeens[units]);
+                        break;
+                    default:
+                        if (units > 0)
+                        {
+                            string word = $"{englishTens[tens]}-{englishUnits[units]}";
+                            words.Add(word);
+                        }
+                        else
+                        {
+                            words.Add(englishTens[tens]);
+                        }
+                        break;
+                }
+
+            tripletEnd:
+                if (!string.IsNullOrEmpty(englishMegas[idx]))
+                {
+                    words.Add(englishMegas[idx]);
+                }
+
+                if ((idx > 0 && triplets[idx - 1] > 0) && (triplet > 0 && triplet < 100))
+                {
+                    words.Add("and");
+                }
+            }
+
+            return string.Join(" ", words);
+        }
+
+        private static string LongToID (long input)
+        {
+            List<string> words = new List<string>();
+
+            if (input < 0)
+            {
+                words.Add("minus");
+                input *= -1;
+            }
+
+            List<long> triplets = LongToTriplets(input);
+
+            if (triplets.Count == 0)
+            {
+                return "nol";
+            }
+
+            for (int idx = triplets.Count - 1; idx >= 0; idx--)
+            {
+                long triplet = triplets[idx];
+
+                if (triplet == 0)
+                {
+                    continue;
+                }
+
+                long hundreds = triplet / 100 % 10;
+                long tens = triplet / 10 % 10;
+                long units = triplet % 10;
+
+                if (hundreds == 1)
+                {
+                    words.Add("seratus");
+                }
+                else if (hundreds > 0)
+                {
+                    words.Add(indonesiaUnits[hundreds]);
+                    words.Add("ratus");
+                }
+
+                if (tens == 0 && units == 0)
+                {
+                    goto tripletEnd;
+                }
+
+                switch (tens)
+                {
+                    case 0:
+                        words.Add(indonesiaUnits[units]);
+                        break;
+                    case 1:
+                        words.Add(indonesiaTeens[units]);
+                        break;
+                    default:
+                        if (units > 0)
+                        {
+                            string word = $"{indonesiaTens[tens]} {indonesiaUnits[units]}";
+                            words.Add(word);
+                        }
+                        else
+                        {
+                            words.Add(indonesiaTens[tens]);
+                        }
+                        break;
+                }
+
+                tripletEnd:
+                if (!string.IsNullOrEmpty(indonesiaMegas[idx]))
+                {
+                    // exception for 1000
+                    if (idx == 1 && triplet == 1)
+                    {
+                        words.RemoveAt(words.Count - 1);
+                        words.Add("seribu");
+                    }
+                    else
+                    {
+                        words.Add(indonesiaMegas[idx]);
+                    }
+                }
+            }
+
+            return string.Join(" ", words);
+        }
+
+        private static List<long> LongToTriplets(long input)
+        {
+            List<long> triplets = new List<long>();
+
+            while (input > 0)
+            {
+                triplets.Add(input % 1000);
+                input /= 1000;
+            }
+
+            return triplets;
         }
     }
 }
